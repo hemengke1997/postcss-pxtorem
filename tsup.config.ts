bac855d76f1428006bc58cde7d0acd2d34e11d56
hemengke1997 <49073282+hemengke1997@users.noreply.github.com>
2022-08-20 12:24:56 +0800

    chore: keep output easy to read

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'tsup'
 
-export const tsup = defineConfig((option) => ({
+export const tsup = defineConfig(() => ({
   entry: ['src/index.ts'],
   dts: true,
   clean: true,
   format: ['cjs', 'esm'],
-  minify: !option.watch,
+  minify: false,
   sourcemap: true,
   esbuildOptions: (options) => {
     options.footer = {
