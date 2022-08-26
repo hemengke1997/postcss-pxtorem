01d52da8ce4096cc8765102afd09438b0c70ae6d
hemengke1997 <49073282+hemengke1997@users.noreply.github.com>
2022-08-26 10:12:18 +0800

    chore: remove map

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'tsup'
 
-export const tsup = defineConfig(() => ({
+export const tsup = defineConfig((option) => ({
   entry: ['src/index.ts'],
   dts: true,
   clean: true,
   format: ['cjs', 'esm'],
   minify: false,
-  sourcemap: true,
+  sourcemap: !!option.watch,
   esbuildOptions: (options) => {
     if (options.format === 'cjs') {
       options.footer = {
