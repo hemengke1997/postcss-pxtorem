f76c8ff81cb10917f65d78c9397488771157c3e3
hemengke1997 <49073282+hemengke1997@users.noreply.github.com>
2023-01-31 11:40:30 +0800

    fix: cjs namedExports

--- tsup.config.ts
+++ tsup.config.ts
@@ -7,11 +7,4 @@ export const tsup = defineConfig((option) => ({
   format: ['cjs', 'esm'],
   minify: false,
   sourcemap: !!option.watch,
-  esbuildOptions: (options) => {
-    if (options.format === 'cjs') {
-      options.footer = {
-        js: 'module.exports = module.exports.default;',
-      }
-    }
-  },
 }))
