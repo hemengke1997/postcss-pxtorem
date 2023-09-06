b0ee498c6bcbcf2f6051b194ce2ed0fb8763ce0f
hemengke1997 <23536175@qq.com>
2023-09-06 10:32:41 +0800

    chore: playground

--- playground/vite-project/vite.config.ts
+++ playground/vite-project/vite.config.ts
@@ -1,7 +1,31 @@
+import type { HtmlTagDescriptor } from 'vite'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { publicTypescript } from 'vite-plugin-public-typescript'
+import manifest from './publicTypescript/manifest.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    publicTypescript(),
+    {
+      name: 'add-script',
+      async transformIndexHtml(html) {
+        const tags: HtmlTagDescriptor[] = [
+          {
+            tag: 'script',
+            attrs: {
+              src: manifest.flexible,
+            },
+            injectTo: 'head-prepend',
+          },
+        ]
+        return {
+          html,
+          tags,
+        }
+      },
+    },
+  ],
 })
