b0fa2637a7df259c9a31061752b0b7539ea200d7
hemengke1997 <23536175@qq.com>
2023-10-26 17:21:54 +0800

    perf: build

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,19 +1,28 @@
-import { defineConfig } from 'tsup'
+import { type Options, defineConfig } from 'tsup'
 
-export const tsup = defineConfig((option) => ({
+const commonConfig = (option: Options): Options => ({
   entry: ['src/index.ts'],
   target: 'node16',
   dts: !option.watch,
   clean: true,
-  format: ['cjs', 'esm'],
   minify: false,
   sourcemap: !!option.watch,
-  esbuildOptions: (options) => {
-    // postcss plugin export default plugin config
-    if (options.format === 'cjs') {
+})
+
+export const tsup = defineConfig((option) => [
+  {
+    format: ['esm'],
+    ...commonConfig(option),
+  },
+  {
+    noExternal: ['decode-uri-component', 'split-on-first'],
+    format: ['cjs'],
+    esbuildOptions: (options) => {
+      // postcss plugin export default plugin config
       options.footer = {
         js: 'module.exports = module.exports.default;',
       }
-    }
+    },
+    ...commonConfig(option),
   },
-}))
+])
